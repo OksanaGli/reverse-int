84679abff3e3983eee73eb0804fff18f5b0fc808
Оксана <l_oksana@list.ru>
2024-10-27 01:22:03 +0300

    feat: solve current task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = reverse;
+function reverse (n) {
+  let changeNumber = String(Math.abs(n));
+  let result = '';
+  for (let i = 0; i < changeNumber.length; i++) {
+    let currentSymbol = changeNumber[i];   
+    result = currentSymbol + result;   
+  }
+  return Number(result);
 }
